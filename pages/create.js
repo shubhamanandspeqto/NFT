0d4c7f53d19535ba26318a8ab00fcb3a213f7821
shubhamanandspeqto <shubham.anand@speqto.com>
2022-03-31 15:40:15 +0530

    added validation and tostifier

--- pages/create.js
+++ pages/create.js
@@ -1,22 +1,31 @@
-import { useState ,useEffect, useMemo  } from 'react'
+import { useState, useEffect, useMemo } from 'react'
+import { useRouter } from 'next/router';
 import { render } from 'react-dom';
 import Header from '../components/Header'
 import { ThirdwebSDK } from '@3rdweb/sdk'
 import { useWeb3 } from '@3rdweb/hooks'
 import { ethers } from "ethers";
 import { client } from '../lib/sanityClient'
+import toast, { Toaster } from 'react-hot-toast'
+
+// import { useForm } from 'react-hook-form';
+// import { yupResolver } from '@hookform/resolvers/yup';
+// import * as Yup from 'yup';
+
 
 export default function CreateItem() {
     const toAddress = "0x69e56D0aF44380BC3B0D666c4207BBF910f0ADC9";
     const { provider } = useWeb3()
+    const [name, setName] = useState("")
+    const [description, setDescription] = useState("")
+    const [price, setPrice] = useState("")
+    const [image, setImage] = useState("")
+    const router = useRouter();
+    const sucessNotify = () => toast.success("Sucess!");
 
-    const[name,setName] = useState("")
-    const[description,setDescription] = useState("")
-    const[price,setPrice] = useState("")
-    const[image,setImage] = useState("")
 
-const fetchCollectionData = async (sanityClient = client) => {
-    const query = `*[_type == "marketItems" && contractAddress == "0x00B70C4aD93336E92eA0517fCA3aC0D0Fd5f5e10" ] {
+    const fetchCollectionData = async (sanityClient = client) => {
+        const query = `*[_type == "marketItems" && contractAddress == "0x00B70C4aD93336E92eA0517fCA3aC0D0Fd5f5e10" ] {
       "imageUrl": profileImage.asset->url,
       "bannerImageUrl": bannerImage.asset->url,
       volumeTraded,
@@ -27,28 +36,28 @@ const fetchCollectionData = async (sanityClient = client) => {
       "allOwners": owners[]->,
       description
     }`
-    const collectionData = await sanityClient.fetch(query)
+        const collectionData = await sanityClient.fetch(query)
 
-    console.log(collectionData, '🔥------------')
-    let contractDataAddress = collectionData[0].contractAddress
-    let contractDataAddress1 = collectionData
-    console.log(contractDataAddress,"contractAddresvfs-------------")
-    console.log(contractDataAddress1,"contractAddresvfs-------------")
-    // the query returns 1 object inside of an array
-    await setCollection(collectionData[0])
+        console.log(collectionData, '🔥------------')
+        let contractDataAddress = collectionData[0].contractAddress
+        let contractDataAddress1 = collectionData
+        console.log(contractDataAddress, "contractAddresvfs-------------")
+        console.log(contractDataAddress1, "contractAddresvfs-------------")
+        // the query returns 1 object inside of an array
+        // await setCollection(collectionData[0])
 
-    
-   
-}
+
+
+    }
+
     useEffect(() => {
         fetchCollectionData()
-        
-        
-      }, [])
-      
+
+    }, [])
+
     const handleChange1 = event => {
         setName(event.target.value);
-       
+
     }
     const handleChange2 = event => {
         setDescription(event.target.value);
@@ -64,32 +73,51 @@ const fetchCollectionData = async (sanityClient = client) => {
     const onChange = (event) => {
         setValue(event.target.value);
     };
-    
+
+    const confirmPurchase = (toastHandler = toast) =>
+        toastHandler.success(`Created successful!`, {
+            style: {
+                background: '#04111d',
+                color: '#fff',
+            },
+        })
+
     function onSubmit(event) {
         event.preventDefault()
-        console.log(image);
-
+        
         let data = {
-            name:name,
+            name: name,
             description: description,
             price: price,
-            image:image,
+            image: image,
         }
-        
-        console.log(data)
+
+        // console.log(data)
         const rpcUrl = "rinkeby";
-    const wallet = new ethers.Wallet(
-        "8f485f65c6efba9bcb5419d0f6283cec90b3e753772d8c0457954306e10edf90",
-        ethers.getDefaultProvider(rpcUrl)
-      );
-    const nft = new ThirdwebSDK(wallet).getNFTModule(
-        "0x00B70C4aD93336E92eA0517fCA3aC0D0Fd5f5e10"
-      );
-     const tx = nft.mintTo(toAddress, data);
-     console.log(tx);
-        }
-        
-        
+        const wallet = new ethers.Wallet(
+            "8f485f65c6efba9bcb5419d0f6283cec90b3e753772d8c0457954306e10edf90",
+            ethers.getDefaultProvider(rpcUrl)
+        );
+        const nft = new ThirdwebSDK(wallet).getNFTModule(
+            "0x00B70C4aD93336E92eA0517fCA3aC0D0Fd5f5e10"
+        );
+        sucessNotify()
+      nft.mintTo(toAddress, data).then(data=>{
+             router.push('/')
+
+        }).catch(err =>{
+            alert("got an error")
+        })
+        // sucessNotify()
+        // console.log("fdfdfdfdfs")
+        // setTimeout(() => {
+        //     router.push('/')
+
+        // }, 3000);
+        // console.log(tx, "tx--------------------");
+    }
+
+
 
     return (
         <>
@@ -100,36 +128,41 @@ const fetchCollectionData = async (sanityClient = client) => {
                         <input
                             placeholder="Asset Name"
                             className="mt-8 border rounded p-4"
-                            name= "name"
+                            name="name"
                             value={name}
                             onChange={handleChange1}
-                        
+                            required
                         />
+
                         <textarea
                             placeholder="Asset Description"
                             className="mt-2 border rounded p-4"
-                            name= "description"
+                            name="description"
                             value={description}
                             onChange={handleChange2}
-                            
+                            required
+                       
+
                         />
                         <input
                             placeholder="Asset Price in Eth"
                             className="mt-2 border rounded p-4"
-                            name= "price"
+                            name="price"
                             value={price}
                             onChange={handleChange3}
-                            
+                            required
+                       
                         />
 
                         <input
                             type="file"
                             name="Asset"
                             className="my-4"
-                            accept = "image/*"
-                            name= "image"
+                            accept="image/*"
+                            name="image"
                             defaultValue={image}
                             onChange={handleChange4}
+                            required
                         />
                         {/* {
                             fileUrl && (
